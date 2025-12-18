4bc6c4ae6239b6be65f874c2486fd847408ea452
ADITYA PATEL <adityapatel8071@gmail.com>
2025-12-18 22:15:41 +0530

    Day 55 completed

---
+++ Day55/scripts.js
@@ -0,0 +1,18 @@
+console.log("hello")
+a=10;
+console.log(a + typeof a);
+console.log(a , typeof a);
+console.log(typeof 10);
+console.log(typeof "hello");
+console.log(typeof 10.5);
+console.log(typeof true);
+console.log(7==8)
+console.log(typeof(null));
+
+const s = {"full name":"aditya", age:18 }
+console.log(s)
+s.bg = "b";
+console.log(s)
+console.log(s["full name"])
+console.log(s.age);
+console.log(s.bg);
