61520ded3b8875364f685b4fd050f3cc6707d227
ADITYA PATEL <adityapatel8071@gmail.com>
2025-12-18 22:16:27 +0530

    Day 60 completed

---
+++ Day60/script.js
@@ -0,0 +1,9 @@
+let str = `aditya patel`
+console.log(str);
+console.log(str.length);
+console.log(str.charAt(0));
+console.log(str[0]);
+console.log(str.charCodeAt(0));
+console.log(str.at(0));
+
+console.log(`2 x 5 = ${2*5}`);
