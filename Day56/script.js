3c652d7c019c9281ae2b019be57eff728f5a0c85
ADITYA PATEL <adityapatel8071@gmail.com>
2025-12-18 22:15:52 +0530

    Day 56 completed

---
+++ Day56/script.js
@@ -0,0 +1,11 @@
+let age = 19;
+if(age>=18) {
+    console.log("You can drive");
+}
+else console.log("You cannot drive"); 
+console.log("hello");
+
+let a = 10, b=12;
+let c = (a>b) ? a : b;
+console.log(c)
+console.log(a<b ? a : b);
